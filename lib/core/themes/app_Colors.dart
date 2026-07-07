504387ca40b500c0e96fd1820e196f4a9dcb8b5f
abdelrahman <aabdelrahmann.sshawqqy.1482000@gmail.com>
2026-07-08 02:21:16 +0300

    init HomeScreen

---
+++ lib/core/themes/app_Colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColorDark = Color(0xff171717);
+  static const Color primaryColor = Color(0xffffffff);
+  static const Color grey = Color(0xff7f7f7f);
+
+}
--- lib/features/news/presentation/screens/home_screen.dart
+++ lib/features/news/presentation/screens/home_screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      height: 300,
-      width: 300,
-      color: Colors.red,
+    return  Scaffold(
+     body: Container(
+       color: Colors.grey,
+     ),
     );
   }
 }
