8086a1dc9e9f512bde5827e871cacac73a8fe82b
abdelrahman <aabdelrahmann.sshawqqy.1482000@gmail.com>
2026-08-18 15:21:59 +0300

    add images path

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -7,4 +7,12 @@ class AppImages {
   static const String themeIcon = "assets/images/theme_icon.png";
   static const String arrow_down_icon = "assets/images/arrow_down_icon.png";
   static const String  language_icon= "assets/images/language_icon.png";
+  static const String  general= "assets/images/general.png";
+  static const String  business= "assets/images/business.png";
+  static const String  sports= "assets/images/sports.png";
+  static const String  technology= "assets/images/technology.png";
+  static const String  science= "assets/images/science.png";
+  static const String  health= "assets/images/health.png";
+  static const String  entertainment= "assets/images/entertainment.png";
+
 }
